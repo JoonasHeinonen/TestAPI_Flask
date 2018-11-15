e796131b0e34b9d60aa3f236ba9df8097e9bd747
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2018-11-15 16:20:42 +0200

    Added a list of objects.

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
+languages = [{'name' : 'JavaScript'}, {'name' : 'Python'}, {'name' : 'Ruby'}]
+
 @app.route("/", methods=['GET'])
 def test():
     return jsonify({'message' : 'It works!'})
+
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
