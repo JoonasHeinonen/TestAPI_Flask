2ea135add10b379269afe5d0bf4c2461f38b9ceb
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2018-11-15 16:17:44 +0200

    Function test returns Json message instead of string.

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
-def hello():
-    return "Hello World!"
+def test():
+    return jsonify({'message' : 'It works!'})
 
 if __name__ == '__main__':
     app.run(debug=True)
