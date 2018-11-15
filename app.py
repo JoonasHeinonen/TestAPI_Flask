49b60aa550010d4ee27dff4ec647c3f033ba1e15
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2018-11-15 16:22:18 +0200

    Added a method returnAll, that returns the names of languages.

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ languages = [{'name' : 'JavaScript'}, {'name' : 'Python'}, {'name' : 'Ruby'}]
 def test():
     return jsonify({'message' : 'It works!'})
 
-    
+@app.route('/lang', methods=['GET'])
+def returnAll():
+    return jsonify({'languages' : languages})
 
 if __name__ == '__main__':
     app.run(debug=True)
