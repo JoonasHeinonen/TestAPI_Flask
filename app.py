903e99f880858d666708cc9a06b53c2419e40a4b
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2018-11-15 16:16:20 +0200

    The app  is able to handle GET requests. Imported jsonify and request.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def hello():
     return "Hello World!"
 
